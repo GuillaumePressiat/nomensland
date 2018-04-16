f1eb2dd3d6644cd2cde00691e71abab87208faad
GuillaumePressiat <guillaume.pressiat@aphp.fr>
2018-04-16 17:11:20 +0200

    Ajout referentiels "1"

---
+++ R/main.R
@@ -0,0 +1,16 @@
+
+#' Liste de tibble référentiels
+#'
+#' @name r
+#' @docType data
+#' @author G. Pressiat
+#' @keywords data
+NULL
+
+#' Liste de requêtes PMSI
+#'
+#' @name l
+#' @docType data
+#' @author G. Pressiat
+#' @keywords data
+NULL
